493894474eb34894974ac4674ce02a563baf6149
shady.afsharii20200 <88148144+shady2022@users.noreply.github.com>
2021-12-26 12:00:41 +0330

    Add files via upload

--- sudoko.py
+++ sudoko.py
@@ -15,10 +15,11 @@ parser.add_argument("--output" , type=str , help="path of your output image")
 
 args = parser.parse_args()
 
-img = cv2.imread(args.input)
+img = cv2.imread("D:\\Python Project\\python_programming\\tamrin8\\sudoku1.jpg")
 gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 blurred_img = cv2.GaussianBlur(gray_img , (args.filter_size,args.filter_size), 3)
+#cv2.imshow("blurred_img", blurred_img)
 
 thresh = cv2.adaptiveThreshold(blurred_img , 255 , cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV , 11 ,2)
 
@@ -43,4 +44,25 @@ else:
     sudoko_img = im(img, sudoku_contour.reshape(4,2))
     warped = im(gray_img, sudoku_contour.reshape(4,2))
     cv2.imshow("sudouko" ,sudoko_img)
-    cv2.imwrite(args.output, sudoko_img)
+    #cv2.imwrite("args.output", sudoko_img)
+
+
+plt.figure(figsize=(20,20))
+plt.subplot(1,3,1)
+plt.imshow(blurred_img ,cmap="gray")
+plt.title("blurred Image")
+plt.axis("off")
+plt.subplot(1,3,2)
+plt.imshow(thresh ,cmap="gray") 
+
+plt.axis("off")
+plt.title("draw contours")
+plt.subplot(1,3,3)
+plt.imshow(result,cmap="gray")
+
+plt.axis("off")
+plt.title("Detected sudoko Image")
+plt.show()
+  
+cv2.waitKey()
+cv2.destroyAllWindows()
